d80548a58322969833661c92b6a6f2e2ae4688fd
Victor Romero <romeroa.victor@yahoo.com>
2020-08-16 23:25:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,8 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  #if hash[key] 
-   # hash[key] =+ 1
+  if hash[key] == nil 
+   hash = {key => 1}
   #el#se
    #hash[key] += 1 
   # given a hash an a key as parameters, return an updated hash
